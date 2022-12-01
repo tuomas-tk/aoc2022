56726533c904486f751c1902ec834cf614fffd4e
Tuomas Karjalainen <tuomas.s.karjalainen@aalto.fi>
2022-12-01 22:33:19 +0200

    Day 1

--- AdventOfCode.Solutions/Year2022/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day01/Solution.cs
@@ -2,15 +2,37 @@ namespace AdventOfCode.Solutions.Year2022.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2022, "") { }
+    public Solution() : base(01, 2022, "Calorie Counting") { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        // Debug = true;
+        int max = 0;
+        foreach (string elf in this.Input.SplitByParagraph())
+        {
+            int totalCalories = elf.SplitByNewline().Select(s => Int32.Parse(s)).Sum();
+            if (totalCalories > max)
+            {
+                max = totalCalories;
+            }
+        }
+        return max.ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        //Debug = true;
+        return Input
+            .SplitByParagraph()
+            .Select(elf =>
+                elf
+                    .SplitByNewline()
+                    .Select(s => Int32.Parse(s))
+                    .Sum()
+            )
+            .Order()
+            .TakeLast(3)
+            .Sum()
+            .ToString();
     }
 }
